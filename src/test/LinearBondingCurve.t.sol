3e4fe3d63754145323414f069fba9eb9602d12ef
Roman Böhringer <r.boehringer@opencore.ch>
2023-11-08 14:03:53 +0100

    Correct casing for import

--- src/test/LinearBondingCurve.t.sol
+++ src/test/LinearBondingCurve.t.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "forge-std/test.sol";
+import "forge-std/Test.sol";
 import "../bonding_curve/LinearBondingCurve.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
